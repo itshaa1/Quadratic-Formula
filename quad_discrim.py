8b2d6ee0c9391285335b31e7b9f0aac10511620a
itshaa1 <59813424+itshaa1@users.noreply.github.com>
2020-01-12 22:20:52 -0500

    Updating Later
    I'll upload the quad_discrim.py file later but this is just a quick first few lines of it

---
+++ quad_discrim.py
@@ -0,0 +1,11 @@
+#importing math module, simplifying radical program, and standard to vertex program
+from math import *
+from simplify_radical import *
+from standard_to_vertex import *
+
+#finding a, b, and c of quadratic equation
+a = int(input("enter a: "))
+b = int(input("enetr b: "))
+c = int(input("enter c: "))
+
+#the quadratic discriminant*(
