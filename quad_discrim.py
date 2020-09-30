776a766abe7ee265fc599748003c76692c41b272
itshaa1 <59813424+itshaa1@users.noreply.github.com>
2020-09-30 12:20:59 -0400

    Code Improvements
    Various improvements and changes, thank you to the people at r/learnpython
    post I used for changes: https://www.reddit.com/r/learnpython/comments/eo4y6k/how_can_i_improve_my_program/

--- quad_discrim.py
+++ quad_discrim.py
@@ -1,49 +1,59 @@
-#importing math module, simplifying radical program, and standard to vertex program
+# importing math module, simplifying radical program, and standard to vertex program
 from math import *
-from simplify_radical import *
-from standard_to_vertex import *
 
-#finding a, b, and c of quadratic equation
-a = int(input("enter a: "))
-b = int(input("enetr b: "))
-c = int(input("enter c: "))
+import sys
 
-#the quadratic discriminant(what's being square rooted)
-quadDiscrim = b ** 2 - 4 * a * c
+from quadratic_functions import pos_simplify, neg_simplify, standard_to_vertex
 
-#this is for the unfactored equation that will be printed
-notReducedTop = str(-b) + " ±sqrt(" + str(quadDiscrim) +  ")" #top of the equation
-notReducedBottom = str(2 * a) #bottom divided by of the equation
-unfactored_equation = notReducedTop + "/" + notReducedBottom #making the equation
+# finding a, b, and c of quadratic equation
+while True:
+    try:
+        a = int(input("enter a: "))
+        b = int(input("enter b: "))
+        c = int(input("enter c: "))
+        break
+    except Exception as e:
+        print(e)
 
-#if a is then you get ZeroDivisionError because its a linear function
+# the quadratic discriminant(what's being square rooted)
+quad_disc = b ** 2 - 4 * a * c
+
+# this is for the unfactored equation that will be printed
+equation_top = f"{-b} ±sqrt({quad_disc})"
+equation_bottom = f"{2 * a}"
+unfactored_equation = f"{equation_top}/{equation_bottom}"
+
+# if a is then you get ZeroDivisionError because its a linear function
+lin_equation = f"y = {b}x + {c}"
 if a == 0:
-    raise ZeroDivisionError("a is 0 so its a linear function, however, the quadratic discriminant is " + str(quadDiscrim) + "  and the equation is " + equation)
+    sys.exit(f"a is 0 so it's a linear function, however, the quadratic discriminant is "
+             f"{quad_disc} and the equation is {lin_equation}")
 
-#if the quadratic discriminant is larger than 0, you will have 2 real answers
-if quadDiscrim > 0:
-    posAnswer = (-b + sqrt(quadDiscrim))/(2 * a) #calculating positive answer
-    negAnswer = (-b - sqrt(quadDiscrim))/(2 * a) #calculating negative answer
-    factored_equation = str(-b) + " ± " + pos_simplify(quadDiscrim) + "/" + notReducedBottom #the factored equation
+# if the quadratic discriminant is larger than 0, you will have 2 real answers
+if quad_disc > 0:
+    pos_answer = (-b + sqrt(quad_disc)) / (2 * a)
+    neg_answer = (-b - sqrt(quad_disc)) / (2 * a)
+    factored_equation = f"{-b} ± {pos_simplify(quad_disc)}/{equation_bottom}"
     print("Two Real Answers")
 
-#if the quadratic discrimant is less than 0, you will have two imaginary answers
-elif quadDiscrim < 0:
-    posAnswer, negAnswer = (-b + quadDiscrim ** 0.5)/(2 * a), (-b - quadDiscrim ** 0.5)/(2 * a) #I tried switchting it up for finding pos and negAnswer on this one, is this a good idea?
-    factored_equation = str(-b) + " ± " + neg_simplify(quadDiscrim) + "/" + notReducedBottom #factored equation
-    print("Two Imgainary Roots")
+# if the quadratic discriminant is less than 0, you will have two imaginary answers
+elif quad_disc < 0:
+    pos_answer, neg_answer = (-b + quad_disc ** 0.5) / (2 * a), (-b - quad_disc ** 0.5) / (2 * a)
+    factored_equation = f"{-b} ± {neg_simplify(quad_disc)}/{equation_bottom}"
+    print("Two Imaginary Roots")
 
-#if the quadratic discriminant is 0, than you will have 1 real answer
+# if the quadratic discriminant is 0, than you will have 1 real answer
 else:
-    posAnswer, negAnswer = (-b)/(2 * a), (-b)/(2 * a) #the one line switch up again
-    factored_equation = str(-b) + "/" + notReducedBottom #just the factored equation
+    answer = (-b) / (2 * a)
+    factored_equation = f"{-b}/{equation_bottom}"
     print("One Real Answer")
 
-#all the ending print statements
-print("The Vertex form of the equation is: " + standard_to_vertex(a, b, c)) #prints vertex form of equation
-print("The positive answer is: " + str(posAnswer)) #prints positive answer
-print("The negative answer is: " + str(negAnswer)) #prints negative answer
-print("The unfactored equation is: " + unfactored_equation) #prints unfactored equation
-print("The factored equation is: " + factored_equation) #prints factored equation
-
-
+# all the ending print statements
+print(f"The Vertex form of the equation is: {standard_to_vertex(a, b, c)}")
+if not quad_disc == 0:
+    print(f"The positive answer is: {pos_answer}")
+    print(f"The negative answer is: {neg_answer}")
+else:
+    print(f"The answer is: {answer}")
+print(f"The unfactored equation is: {unfactored_equation}")
+print(f"The factored equation is: {factored_equation}")
